a6fa952e7d6e6f0b9bb45932f7cdb9a2766e60af
Ben T <ben.trevethan@gmail.com>
2021-03-04 16:50:37 +1030

    testing populating data in seed

--- seed.sql
+++ seed.sql
@@ -1,1 +1,24 @@
-INSERT INTO 
+INSERT INTO employee (first_name, last_name)
+VALUES ('Ben', 'Trevethan'),
+('John', 'Smith'),
+('Barry', 'Johnson'),
+('Felicity', 'Jones'),
+('Lee', 'Costello'),
+('Charlie', 'Figaro');
+
+INSERT INTO department (department_name)
+VALUES ('Sales'),
+('Engineering'),
+('Admin/HR'),
+('Intern Program')
+('Management')
+('CEO');
+
+INSERT INTO role (title, salary)
+VALUES ('Manager', 150000),
+('Salesperson', 80000),
+('Admin Clerk', 72000),
+('HR Representative', 83000),
+('Engineer', 128000),
+('CEO', 420000),
+('Intern', 43500);
