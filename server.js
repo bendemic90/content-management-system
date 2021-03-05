ef49ed14db31f3807566e628edf3ab5fcd0847a9
Ben T <ben.trevethan@gmail.com>
2021-03-05 16:30:20 +1030

    restructuring again to remove sequelize

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+const mysql = require('mysql');
 const express = require('express');
-//const routes = require('./routes');
-const sequelize = require('./config/connection');
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: 'root',
+
+  // Be sure to update with your own MySQL password!
+  password: process.env.DB_PASS,
+  database: 'cms_db',
+});
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,10 +23,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// turn on routes
-//app.use(routes);
-
-// turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}\non port: ${PORT}`);
+  });
